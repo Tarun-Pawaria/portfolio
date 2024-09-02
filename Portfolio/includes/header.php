4149936503ad2e874d498ca0c6c158a9d24f055e
Tarun-Pawaria <tarunpawaria830@gmail.com>
2024-09-02 21:34:52 +0530

    Add files via upload

---
+++ Portfolio/includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Tarun Pawaria</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/header.css'>
+    <!--<script src='main.js'></script>-->
+</head>
+<body>
+
+<div id="head"> 
+    <h1 id="heading">Portfolio</h1>
+
+</div>
+
+
+<div id="nav">
+    <ul>
+        <li><a class="h" href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="project.php">Project</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</div>
---
+++ Portfolio/index.php
@@ -0,0 +1,56 @@
+
+
+<?php include_once 'includes/header.php'; ?>
+    
+
+<link rel='stylesheet' type='text/css' media='screen' href='css/index.css'>
+
+
+<div id="portfolio">
+<div id="image-section1">
+    <img id="image" src="images/IMG.jpg">
+</div>
+<div id="content-section">
+    <div id="name-section">
+    <h1>I'm Tarun Pawaria</h1>
+        <h2>Web Developer</h2>
+
+    </div>
+    <div id="phase">
+        <p>"I am eager to enhance my skill set and I am open to guidance and training to achieve this. 
+        I believe that continuous learning is essential for my professional growth and I am committed to it."</p>
+    </div>
+    <div>
+    <a href="https://wa.me/8307853764"><img src="images/whatsapp.png" alt="whatapp link"></a>
+    <a href="mailto:tarunpawaria921@gmail.com"><img src="images/email.png" style="height: 100px;" alt="tarunpawaria921@gmail.com"></a>
+</div>
+</div>
+<div id="image-section">https://github.com/Tarun-Pawaria/portfolio
+    <img id="image" src="images/IMG.jpg">
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
