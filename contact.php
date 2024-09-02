4082dcabb84d8dda579427a5c516ea5647c52759
Tarun-Pawaria <tarunpawaria830@gmail.com>
2024-09-02 21:11:16 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,27 @@
+<?php include_once 'includes/header.php'; ?>
+
+
+<br><br><br><br>
+<div id="con">
+    
+    <h1 id="head1">Whatsapp :</h1><a href="https://wa.me/8307853764"><img src="images/whatsapp.png"></a>
+</div>
+
+<br><br><br><br>
+<div id="em">
+    <h1>Email : </h1><a href="mailto:tarunpawaria921@gmail.com"><img src="images/email.png" style="height: 100px;"></a>
+</div>
+
+
+
+
+
+<style>
+    #con,#em{
+       display: flex;
+       padding: 20px;
+    }
+    #head1{
+        margin-top: 10px;
+    }
+</style>
